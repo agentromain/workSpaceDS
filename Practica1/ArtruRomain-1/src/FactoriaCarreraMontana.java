0de65ba57c37acfd2cfe12a14ea8861f2c0e722e
agentromain <romain.artru@epfl.ch>
2017-02-28 15:59:22 +0100

    details

--- Practica1/ArtruRomain-1/src/FactoriaCarreraMontana.java
+++ Practica1/ArtruRomain-1/src/FactoriaCarreraMontana.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
  * @author romainartru
  *
  */
-public class FactoriaCarreraMontana extends FactoriaCarrera{
+public class FactoriaCarreraMontana implements Carrera{
 	public static ArrayList<Bicicleta> crearCM(int n){
     	ArrayList<Bicicleta> participantes = new ArrayList<>();
     	for(int i = 0; i < n; ++i){
