c1f2a076f56cc719cae416ba3194298334c388cc
agentromain <romain.artru@epfl.ch>
2017-03-01 19:03:41 +0100

    create exercice 2

---
+++ Practica1/ArtruRomain-2/src/Pantalla.java
@@ -0,0 +1,15 @@
+import java.util.Observable;
+import java.util.Observer;
+
+public class Pantalla implements Observer {
+
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void refrescarPantalla(){
+		
+	}
+}
---
+++ Practica1/ArtruRomain-2/src/Simulador.java
@@ -0,0 +1,26 @@
+import java.util.Observable;
+import java.util.Random;
+
+public class Simulador extends Observable implements Runnable {
+	ObservablePantalla oP;
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		Random r= new Random(System.currentTimeMillis());
+		int temperatura;
+
+		while (true){
+
+		  temperatura= r.nextInt();
+
+		  try {Thread.sleep(60);}
+		  catch(java.lang.InterruptedException e){
+		    e.printStackTrace();
+		  }
+
+		  oP.notifyObservers();
+
+		}
+	}
+}
